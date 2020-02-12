c25f3b8e964f73bdaef408c7058028a929c5aca2
Wildan Firdaus <firdaus@alterra.id>
2020-02-12 17:26:00 +0700

    menambahkan pencarian berdasarkan nama pengguna

--- blueprints/pengguna/model.py
+++ blueprints/pengguna/model.py
@@ -1,114 +1,7 @@
+from sqlalchemy.orm import relationship
 from blueprints import db
 from flask_restful import fields
 from datetime import datetime
-
-
-class Pengguna(db.Model):
-    __tablename__ = "pengguna"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    avatar = db.Column(db.String(1000), nullable=False, default="")
-    ktp = db.Column(db.String(1000), nullable=False, default="")
-    nama_depan = db.Column(db.String(100), nullable=False, default="")
-    nama_belakang = db.Column(db.String(100), nullable=False, default="")
-    kota = db.Column(db.String(20), nullable=False, default="")
-    email = db.Column(db.String(100), nullable=False, default="")
-    kata_sandi = db.Column(db.String(100), nullable=False, default="")
-    telepon = db.Column(db.String(15), nullable=False, default="")
-    nomor_pln = db.Column(db.String(20), nullable=False, default="")
-    nomor_bpjs = db.Column(db.String(20), nullable=False, default="")
-    nomor_telkom = db.Column(db.String(20), nullable=False, default="")
-    nomor_pdam = db.Column(db.String(20), nullable=False, default="")
-    aktif = db.Column(db.Boolean, nullable=False, default=True)
-    terverifikasi = db.Column(db.Boolean, nullable=False, default=False)
-    dibuat = db.Column(db.DateTime, nullable=False)
-    diperbarui = db.Column(db.DateTime, nullable=False)
-
-    respons = {
-        "dibuat": fields.DateTime(dt_format="iso8601"),
-        "diperbarui": fields.DateTime(dt_format="iso8601"),
-        "id": fields.Integer,
-        "avatar": fields.String,
-        "ktp": fields.String,
-        "nama_depan": fields.String,
-        "nama_belakang": fields.String,
-        "kota": fields.String,
-        "email": fields.String,
-        "telepon": fields.String,
-        "nomor_pln": fields.String,
-        "nomor_bpjs": fields.String,
-        "nomor_telkom": fields.String,
-        "nomor_pdam": fields.String,
-        "aktif": fields.Boolean,
-        "terverifikasi": fields.Boolean
-    }
-
-    respons_jwt = {
-        "id": fields.Integer,
-        "kota": fields.String,
-        "terverifikasi": fields.Boolean
-    }
-
-    def __init__(self, nama_depan, nama_belakang, kota, email, kata_sandi, telepon):
-        self.nama_depan = nama_depan
-        self.nama_belakang = nama_belakang
-        self.kota = kota
-        self.email = email
-        self.kata_sandi = kata_sandi
-        self.telepon = telepon
-        self.dibuat = datetime.now()
-        self.diperbarui = datetime.now()
-
-    def __repr__(self):
-        return "<Pengguna %r>" % self.id
-
-
-class Keluhan(db.Model):
-    __tablename__ = "keluhan"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    id_pengguna = db.Column(db.Integer, db.ForeignKey('pengguna.id'), nullable=False)
-    foto_sebelum = db.Column(db.String(1000), nullable=False, default="")
-    foto_sesudah = db.Column(db.String(1000), nullable=False, default="")
-    kota = db.Column(db.String(20), nullable=False, default="")
-    longitude = db.Column(db.String(100), nullable=False, default="")
-    latitude = db.Column(db.String(100), nullable=False, default="")
-    isi = db.Column(db.String(1000), nullable=False, default="")
-    status = db.Column(db.String(10), nullable=False, default="diterima")
-    dibaca = db.Column(db.Boolean, nullable=False, default=True)
-    anonim = db.Column(db.Boolean, nullable=False, default=False)
-    kepuasan = db.Column(db.Boolean)
-    dibuat = db.Column(db.DateTime, nullable=False)
-    diperbarui = db.Column(db.DateTime, nullable=False)
-
-    respons = {
-        "dibuat": fields.DateTime(dt_format="iso8601"),
-        "diperbarui": fields.DateTime(dt_format="iso8601"),
-        "id": fields.Integer,
-        "id_pengguna": fields.Integer,
-        "foto_sebelum": fields.String,
-        "foto_sesudah": fields.String,
-        "kota": fields.String,
-        "longitude": fields.String,
-        "latitude": fields.String,
-        "isi": fields.String,
-        "status": fields.String,
-        "dibaca": fields.Boolean,
-        "anonim": fields.Boolean,
-        "kepuasan": fields.Boolean
-    }
-
-    def __init__(self, id_pengguna, foto_sebelum, kota, longitude, latitude, isi, anonim):
-        self.id_pengguna = id_pengguna
-        self.foto_sebelum = foto_sebelum
-        self.kota = kota
-        self.longitude = longitude
-        self.latitude = latitude
-        self.isi = isi
-        self.anonim = anonim
-        self.dibuat = datetime.now()
-        self.diperbarui = datetime.now()
-
-    def __repr__(self):
-        return "<Keluhan %r>" % self.id
 
 
 class KomentarKeluhan(db.Model):
@@ -152,6 +45,8 @@ class DukungKeluhan(db.Model):
     id_keluhan = db.Column(db.Integer, db.ForeignKey('keluhan.id'), nullable=False)
     dibuat = db.Column(db.DateTime, nullable=False)
     diperbarui = db.Column(db.DateTime, nullable=False)
+    pengguna = relationship("Pengguna", back_populates="dukung_keluhan")
+    keluhan = relationship("Keluhan", back_populates="dukung_keluhan")
 
     respons = {
         "dibuat": fields.DateTime(dt_format="iso8601"),
@@ -169,3 +64,117 @@ class DukungKeluhan(db.Model):
 
     def __repr__(self):
         return "<Dukung Keluhan %r>" % self.id
+
+
+class Keluhan(db.Model):
+    __tablename__ = "keluhan"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_pengguna = db.Column(db.Integer, db.ForeignKey('pengguna.id'), nullable=False)
+    foto_sebelum = db.Column(db.String(1000), nullable=False, default="")
+    foto_sesudah = db.Column(db.String(1000), nullable=False, default="")
+    kota = db.Column(db.String(20), nullable=False, default="")
+    longitude = db.Column(db.String(100), nullable=False, default="")
+    latitude = db.Column(db.String(100), nullable=False, default="")
+    isi = db.Column(db.String(1000), nullable=False, default="")
+    status = db.Column(db.String(10), nullable=False, default="diterima")
+    dibaca = db.Column(db.Boolean, nullable=False, default=True)
+    anonim = db.Column(db.Boolean, nullable=False, default=False)
+    kepuasan = db.Column(db.Boolean)
+    dibuat = db.Column(db.DateTime, nullable=False)
+    diperbarui = db.Column(db.DateTime, nullable=False)
+    # pengguna = relationship("Pengguna", secondary=DukungKeluhan, back_populates="keluhan")
+    pengguna = relationship("Pengguna", back_populates="keluhan")
+    dukung_keluhan = relationship("DukungKeluhan", back_populates="keluhan")
+
+    respons = {
+        "dibuat": fields.DateTime(dt_format="iso8601"),
+        "diperbarui": fields.DateTime(dt_format="iso8601"),
+        "id": fields.Integer,
+        "id_pengguna": fields.Integer,
+        "foto_sebelum": fields.String,
+        "foto_sesudah": fields.String,
+        "kota": fields.String,
+        "longitude": fields.String,
+        "latitude": fields.String,
+        "isi": fields.String,
+        "status": fields.String,
+        "dibaca": fields.Boolean,
+        "anonim": fields.Boolean,
+        "kepuasan": fields.Boolean
+    }
+
+    def __init__(self, id_pengguna, foto_sebelum, kota, longitude, latitude, isi, anonim):
+        self.id_pengguna = id_pengguna
+        self.foto_sebelum = foto_sebelum
+        self.kota = kota
+        self.longitude = longitude
+        self.latitude = latitude
+        self.isi = isi
+        self.anonim = anonim
+        self.dibuat = datetime.now()
+        self.diperbarui = datetime.now()
+
+    def __repr__(self):
+        return "<Keluhan %r>" % self.id
+
+
+class Pengguna(db.Model):
+    __tablename__ = "pengguna"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    avatar = db.Column(db.String(1000), nullable=False, default="")
+    ktp = db.Column(db.String(1000), nullable=False, default="")
+    nama_depan = db.Column(db.String(100), nullable=False, default="")
+    nama_belakang = db.Column(db.String(100), nullable=False, default="")
+    kota = db.Column(db.String(20), nullable=False, default="")
+    email = db.Column(db.String(100), nullable=False, default="")
+    kata_sandi = db.Column(db.String(100), nullable=False, default="")
+    telepon = db.Column(db.String(15), nullable=False, default="")
+    nomor_pln = db.Column(db.String(20), nullable=False, default="")
+    nomor_bpjs = db.Column(db.String(20), nullable=False, default="")
+    nomor_telkom = db.Column(db.String(20), nullable=False, default="")
+    nomor_pdam = db.Column(db.String(20), nullable=False, default="")
+    aktif = db.Column(db.Boolean, nullable=False, default=True)
+    terverifikasi = db.Column(db.Boolean, nullable=False, default=False)
+    dibuat = db.Column(db.DateTime, nullable=False)
+    diperbarui = db.Column(db.DateTime, nullable=False)
+    # keluhan = relationship("Keluhan", secondary=DukungKeluhan, back_populates="pengguna")
+    keluhan = relationship("Keluhan", back_populates="pengguna")
+    dukung_keluhan = relationship("DukungKeluhan", back_populates="pengguna")
+
+    respons = {
+        "dibuat": fields.DateTime(dt_format="iso8601"),
+        "diperbarui": fields.DateTime(dt_format="iso8601"),
+        "id": fields.Integer,
+        "avatar": fields.String,
+        "ktp": fields.String,
+        "nama_depan": fields.String,
+        "nama_belakang": fields.String,
+        "kota": fields.String,
+        "email": fields.String,
+        "telepon": fields.String,
+        "nomor_pln": fields.String,
+        "nomor_bpjs": fields.String,
+        "nomor_telkom": fields.String,
+        "nomor_pdam": fields.String,
+        "aktif": fields.Boolean,
+        "terverifikasi": fields.Boolean
+    }
+
+    respons_jwt = {
+        "id": fields.Integer,
+        "kota": fields.String,
+        "terverifikasi": fields.Boolean
+    }
+
+    def __init__(self, nama_depan, nama_belakang, kota, email, kata_sandi, telepon):
+        self.nama_depan = nama_depan
+        self.nama_belakang = nama_belakang
+        self.kota = kota
+        self.email = email
+        self.kata_sandi = kata_sandi
+        self.telepon = telepon
+        self.dibuat = datetime.now()
+        self.diperbarui = datetime.now()
+
+    def __repr__(self):
+        return "<Pengguna %r>" % self.id
--- blueprints/umum/__init__.py
+++ blueprints/umum/__init__.py
@@ -1,3 +1,4 @@
+from sqlalchemy import func, or_
 from flask import Blueprint
 from flask_restful import Api, Resource, reqparse, marshal
 from flask_jwt_extended import create_access_token
@@ -103,9 +104,8 @@ class UmumKeluhan(Resource):
     # menampilkan semua keluhan pengguna
     def get(self, id=None):
         if id is None:
-            daftar_keluhan = []
             parser = reqparse.RequestParser()
-            parser.add_argument("id_keluhan", location="args")
+            parser.add_argument("kata_kunci", location="args")
             parser.add_argument("kota", location="args", required=True)
             parser.add_argument(
                 "status", location="args",
@@ -131,20 +131,21 @@ class UmumKeluhan(Resource):
             parser.add_argument("per_halaman", type=int, location="args", default=10)
             args = parser.parse_args()
             
-            data = db.session.query(Pengguna, Keluhan, DukungKeluhan).join(Keluhan).join(DukungKeluhan)
-            data = data.filter(Pengguna.nama_depan.like("%"+"ch"+"%")).all()
-            # data = db.session.query(Keluhan, Pengguna).join(Pengguna).filter_by(kota=args["kota"]).all()
-            # data = db.session.query(Pengguna, Keluhan).join(Keluhan).filter_by(kepuasan=True).all()
-            # [data_pengguna, data_keluhan] = data[-1]
-            [data_keluhan, data_pengguna] = data[-1]
-            print(len(data))
-            print(data_pengguna.id, data_pengguna.email, data_keluhan.id_pengguna, data_keluhan.isi)
-            # print(marshal(data_keluhan, Keluhan.respons))
+            filter_keluhan = db.session.query(
+                Pengguna.nama_depan,
+                Pengguna.nama_belakang,
+                func.count(DukungKeluhan.id),
+                Keluhan
+            ).join(Pengguna, Pengguna.id==Keluhan.id_pengguna)\
+            .outerjoin(DukungKeluhan, DukungKeluhan.id_keluhan==Keluhan.id).group_by(Keluhan.id)
             # filter berdasarkan kota
-            filter_keluhan = Keluhan.query.filter_by(kota=args["kota"])
+            filter_keluhan = filter_keluhan.filter(Keluhan.kota==args["kota"])
             # filter id berdasarkan id_keluhan
-            if args["id_keluhan"]:
-                filter_keluhan = filter_keluhan.filter(Keluhan.id.like(args["id_keluhan"]+"%"))
+            if args["kata_kunci"]:
+                filter_keluhan = filter_keluhan.filter(or_(
+                    (Pengguna.nama_depan+" "+Pengguna.nama_belakang).like("%"+args["kata_kunci"]+"%"),
+                    Keluhan.id.like(args["kata_kunci"]+"%")
+                ))
             # filter berdasarkan status keluhan
             if args["status"]:
                 filter_keluhan = filter_keluhan.filter(Keluhan.status.like("%"+args["status"]+"%"))
@@ -188,15 +189,14 @@ class UmumKeluhan(Resource):
                 "total_keluhan": total_keluhan, "halaman":args["halaman"],
                 "total_halaman":total_halaman, "per_halaman":args["per_halaman"]
             }
+            daftar_keluhan = []
             for setiap_keluhan in filter_keluhan.all():
-                data_keluhan = {}
-                # mengambil nama pengguna pada setiap keluhan
-                id_pengguna = setiap_keluhan.id_pengguna
-                data_pengguna = Pengguna.query.get(id_pengguna)
-                data_keluhan["nama_depan"] = data_pengguna.nama_depan
-                data_keluhan["nama_belakang"] = data_pengguna.nama_belakang
-                # mengambil detail keluhan
-                data_keluhan["detail_keluhan"] = marshal(setiap_keluhan, Keluhan.respons)
+                data_keluhan = {
+                    "nama_depan": setiap_keluhan[0],
+                    "nama_belakang": setiap_keluhan[1],
+                    "total_dukungan": setiap_keluhan[2],
+                    "detail_keluhan": marshal(setiap_keluhan[3], Keluhan.respons)
+                }
                 daftar_keluhan.append(data_keluhan)
             respons_keluhan["daftar_keluhan"] = daftar_keluhan
             return respons_keluhan, 200, {"Content-Type": "application/json"}
