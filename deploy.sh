9dc6a4a8b1340e169330dc23a782aff2b566751f
Wildan Firdaus <firdaus@alterra.id>
2020-02-14 19:10:24 +0700

    memperbaiki nama variable pada env

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,3 @@ docker stop lokesal-backend
 docker rm lokesal-backend
 docker rmi wiflash/lokesal:be-latest
 docker run -d --name lokesal-backend -p 5000:5000 wiflash/lokesal:be-latest
-echo $INI_KUNCI_LOKESAL >> ~/coba.txt
-echo $INI_UNAME >> ~/coba.txt
-echo $INI_PWD >> ~/coba.txt
-echo $INI_DB_TEST >> ~/coba.txt
-echo $INI_DB_DEV >> ~/coba.txt
-echo $INI_DB_ENDPOINT >> ~/coba.txt
